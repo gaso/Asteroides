0f623394539e03aff184bde83f2a8ce400c6f5aa
gaso <gustavo2840@hotmail.com>
2013-02-19 23:07:38 -0500

    Agregando Soporte Orientacion Horizontal Ventana Principal

--- gen/co/net/gidsoft/asteroides/R.java
+++ gen/co/net/gidsoft/asteroides/R.java
@@ -27,9 +27,9 @@ public final class R {
         public static final int Button01=0x7f080002;
         public static final int Button02=0x7f080003;
         public static final int Button03=0x7f080004;
-        public static final int LinearLayout1=0x7f080000;
         public static final int action_settings=0x7f080005;
         public static final int button1=0x7f080001;
+        public static final int main_LinearLayout=0x7f080000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
