f4ed1405823dd96e8ac9409600df8021e1995b09
gaso <gustavo2840@hotmail.com>
2013-03-27 11:47:53 -0500

    Agregando Escucha al Seleccionar elemento de una Lista

--- src/co/net/gidsoft/asteroides/Puntuaciones.java
+++ src/co/net/gidsoft/asteroides/Puntuaciones.java
@@ -2,47 +2,59 @@ package co.net.gidsoft.asteroides;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Puntuaciones extends ListActivity {
 
-// Lista Generica con Strings
-//	@Override 
-//	public void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.puntuaciones);
-//		setListAdapter(new ArrayAdapter<String>(this,
-//				android.R.layout.simple_list_item_1,
-//				Asteroides.almacen.listaPuntuaciones(10)));
-//	}
-	
-//Lista con Imagenes y View Personalizada	
-//	@Override
-//	public void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.puntuaciones);
-//		 setListAdapter(
-//                 new ArrayAdapter<String>(this,
-//                       R.layout.elemento_lista,
-//                 R.id.titulo,
-//                       Asteroides.almacen.listaPuntuaciones(10)));
-//	}
-	
+	// Lista Generica con Strings
+	// @Override
+	// public void onCreate(Bundle savedInstanceState) {
+	// super.onCreate(savedInstanceState);
+	// setContentView(R.layout.puntuaciones);
+	// setListAdapter(new ArrayAdapter<String>(this,
+	// android.R.layout.simple_list_item_1,
+	// Asteroides.almacen.listaPuntuaciones(10)));
+	// }
+
+	// Lista con Imagenes y View Personalizada
+	// @Override
+	// public void onCreate(Bundle savedInstanceState) {
+	// super.onCreate(savedInstanceState);
+	// setContentView(R.layout.puntuaciones);
+	// setListAdapter(
+	// new ArrayAdapter<String>(this,
+	// R.layout.elemento_lista,
+	// R.id.titulo,
+	// Asteroides.almacen.listaPuntuaciones(10)));
+	// }
+
 	/**
-	 * NOTA:
-	 * ============
-	 * En algunos casos el adaptador ha de trabajar con listas muy grandes o 
-	 *  estas listas han de ser creadas desde un servidor. 
-	 *  En estos casos es mejor ir solicitando la información a 
-	 *  medida que se va representando. Un ejemplo se muestra en la aplicación 
-	 *  ApiDemos descrita en el capítulo 1, en la actividad:
-	 *  com.example.android.apis.view.List13 
+	 * NOTA: En algunos casos el adaptador ha de trabajar con listas muy grandes
+	 * o estas listas han de ser creadas desde un servidor. En estos casos es
+	 * mejor ir solicitando la información a medida que se va representando. Un
+	 * ejemplo se muestra en la aplicación ApiDemos descrita en el capítulo 1,
+	 * en la actividad: com.example.android.apis.view.List13
 	 */
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.puntuaciones);
-		 setListAdapter(new MiAdaptador(this, Asteroides.almacen.listaPuntuaciones(10))  );
+		setListAdapter(new MiAdaptador(this,
+				Asteroides.almacen.listaPuntuaciones(10)));
+	}
+
+	@Override
+	protected void onListItemClick(ListView listView, View view, int position,
+			long id) {
+		super.onListItemClick(listView, view, position, id);
+		Object o = getListAdapter().getItem(position);
+		Toast.makeText(
+				this,
+				"Selección: " + Integer.toString(position) + " - "
+						+ o.toString(), Toast.LENGTH_LONG).show();
 	}
 
 }
